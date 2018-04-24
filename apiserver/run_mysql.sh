b9e6d8363da5dccf048a24215799706c61e0123a
Ubuntu <ubuntu@ip-172-31-34-102.eu-central-1.compute.internal>
2018-04-24 13:42:35 +0000

    d2u

--- apiserver/run_mysql.sh
+++ apiserver/run_mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -d -v $(pwd)/../db:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 44330:3306 halite_sql
+docker run --net=host --rm -d -v $(pwd)/../db:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 3306:3306 halite_sql
